66dbb6430cb3fa7c1af4a907e4e24e36fd1e35a5
prajvalyelpale <prajvaldyelpale@gmail.com>
2024-07-17 19:32:52 +0530

    Infocards added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
-// import About from './components/About'
-// import Carousel from './components/Carousel'
+
 import Navbar from './components/Navbar'
 // import Footer from './components/Footer'
 // import {slides} from './data/carouselData.json'
@@ -10,7 +9,7 @@ import { Login } from './components/pages/Login'
 import JobDetail from './components/jobDetails'
 import Jobs from './components/pages/Jobs'
 import Swarozgar from './components/pages/Swarozgar'
-import ContactUs from './components/pages/ContactUs'
+
 import { Signup } from './components/pages/Signup'
 import Courses from './components/pages/Courses'
 // import Mentors from './components/Mentors'
@@ -27,9 +26,7 @@ function App() {
       <Route exact path='/Signup' element={<Signup />}/>
       <Route exact path='/Jobs' element={<Jobs />}/>
       <Route exact path='/Swarozgar' element={<Swarozgar />}/>
-      <Route exact path='/ContactUs' element={<ContactUs />}/>
       <Route exact path='/Courses' element={<Courses />}/>
-      {/* <Route path="/mentors" element={<Mentors />} /> */}
       <Route path="/job/:id" element={<JobDetail />} />
       
     </Routes>
